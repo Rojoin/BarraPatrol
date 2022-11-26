4ecc99b28d8a39daef84b49b1e4a2d1950596997
Rojoin <nachoarrastua@gmail.com>
2022-11-26 18:56:36 -0300

    Change Player Class to use fewer variables and Methods

--- src/gameObjects/player.h
+++ src/gameObjects/player.h
@@ -6,30 +6,27 @@
 
 class Player
 {
-public:
-    Player(Rectangle body, std::string name, float speed);
-    Player();
-    ~Player();
-    void MoveRight();
-    void MoveLeft();
-    void Jump();
-    void MoveDown();
-    Bullet ShootUp() const;
-    Bullet ShootRight() const;
-    void Draw(Texture2D playerTexture) const;
-    Rectangle GetBody() const;
-    void SetY(float y_);
-    void SetX(float x_);
-    void SetHp(int hpModifier);
-    void SetSpeed(float speed_);
 private:
-    int hp;
-    bool isAlive;
-    bool isJumping;
-    float jumpTime = 0.18f; 
-    float jumpTimer; 
-    float speed;
-    bool IsGrounded() const;
-    Rectangle body{};
-    std::string name;
+
+	bool deadState;
+	bool jumpState;
+	float jumpTime = 0.18f;
+	float jumpTimer;
+	float speed;
+	Texture2D texture;
+	bool IsGrounded() const;
+	Rectangle body;
+
+public:
+	Player(Rectangle body, float speed);
+	Player();
+	~Player();
+	void MoveRight();
+	void MoveLeft();
+	void jump();
+	void MoveDown();
+	Bullet ShootUp() const;
+	void draw();
+	Rectangle getBody() const;
+
 };
