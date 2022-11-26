78609175a95d6a088e1a27c1bd5c6539049d1330
Rojoin <nachoarrastua@gmail.com>
2022-11-26 15:23:48 -0300

    Add statesManager Class Constructor & Destructor Definitions

--- src/gameScreens/statesManager.cpp
+++ src/gameScreens/statesManager.cpp
@@ -1,1 +1,25 @@
 #include "statesManager.h"
+
+#include <ctime>
+
+#include "GameStates.h"
+
+statesManager::statesManager()
+{
+
+	this->isProgramRunning = true;
+	setGameState(GameStates::Menu);
+	SetRandomSeed(static_cast<unsigned int>(time(NULL)));
+	InitWindow(1024, 768, "Tactical Llama");
+	HideCursor();
+	InitAudioDevice();
+	loadResources();
+	SetExitKey(NULL);
+	SetWindowMinSize(1024, 768);
+}
+
+statesManager::~statesManager()
+{
+	
+}
+
