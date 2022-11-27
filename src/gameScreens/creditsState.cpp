5d30bc6c5758e98bc81295ddb86aab81359ef17d
Rojoin <nachoarrastua@gmail.com>
2022-11-27 16:45:54 -0300

    Add creditsState.h & creditsState.cpp

---
+++ src/gameScreens/creditsState.cpp
@@ -0,0 +1,39 @@
+#include <raylib.h>
+#include "creditsState.h"
+#include  "gameStates.h"
+#include "system/draw.h"
+#include "system/button.h"
+
+extern Font customFont;
+Texture2D creditsTexture;
+extern Button escapeButton;
+void statesCredits(GameStates& gameStates)
+{
+	Vector2 mousePoint = GetMousePosition();
+	escapeButton = createButton(0, (float)GetScreenHeight() - escapeButton.rec.height * 2, escapeButton.buttonTittle, escapeButton.color);
+	if (CheckCollisionPointRec(mousePoint, escapeButton.rec))
+	{
+		escapeButton.isOverThisButton = true;
+
+		if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+		{
+			gameStates = setGameState(GameStates::Menu);
+		}
+	}
+	else
+	{
+		escapeButton.isOverThisButton = false;
+	}
+}
+void drawCredits()
+{
+
+	float width = static_cast<float>(GetScreenWidth());
+	ClearBackground(BLACK);
+	drawText("Game Made By Ignacio Arrastua", 500, 500, 40, RED);
+	float scale = 0.12f * GetScreenHeight() / 768;
+	drawTexture(creditsTexture, { width / 2.0f -(creditsTexture.width/2.0f)* scale, 0}, 0, scale, WHITE);
+	drawButton(escapeButton);
+
+
+}
---
+++ src/gameScreens/creditsState.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "GameStates.h"
+
+void statesCredits(GameStates& gameStates);
+void drawCredits();
