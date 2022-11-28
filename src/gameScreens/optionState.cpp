9500470253d76cdf6146a8b22e3f7bccc3963fdf
Rojoin <nachoarrastua@gmail.com>
2022-11-27 22:57:27 -0300

    Delete ScreenResolution logic

--- src/gameScreens/optionState.cpp
+++ src/gameScreens/optionState.cpp
@@ -7,15 +7,15 @@
 
 Button backButton = createButton(" GO BACK", RED);
 Button fullScreen;
-Button changeScreen;
-static int resolutionIndex = 1;
-static int maxResolutionIndex = 4;
+
+
+Vector2 screenSize = {1, 1}; 
 void statesOptions(GameStates& gameStates)
 {
 
 	backButton = createButton(0, (float)GetScreenHeight() - backButton.rec.height * 2, backButton.buttonTittle, backButton.color);
 	fullScreen = createButton(static_cast<float>(GetScreenWidth() / 2), static_cast<float>(GetScreenHeight() / 2), 200 , 60.0f * static_cast<float>(GetScreenHeight() / 768), "FullScreen", RED);
-	changeScreen = createButton(static_cast<float>(GetScreenWidth() / 4), static_cast<float>(GetScreenHeight() / 2), 200, 60.0f * static_cast<float>(GetScreenHeight() / 768), "Change Res", WHITE);
+	
 	Vector2 mousePoint = GetMousePosition();
 	if (CheckCollisionPointRec(mousePoint, backButton.rec))
 	{
@@ -45,52 +45,13 @@ void statesOptions(GameStates& gameStates)
 		fullScreen.isOverThisButton = false;
 	}
 
-	if (GetScreenWidth() < 2560)
-	{
-		maxResolutionIndex = 3;
-	}
-	else if (GetScreenWidth() < 1920)
-	{
-		maxResolutionIndex = 2;
-	}
-	else if (GetScreenWidth() < 1600)
-	{
-		maxResolutionIndex = 1;
-	}
 
-	if (CheckCollisionPointRec(mousePoint, changeScreen.rec))
-	{
-		changeScreen.isOverThisButton = true;
+	
 
-		if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
-		{
-			resolutionIndex++;
-			if (resolutionIndex > maxResolutionIndex)
-			{
-				resolutionIndex = 1;
-			}
-		}
-	}
-	else
-	{
-		changeScreen.isOverThisButton = false;
-	}
+		float height = 1;
+		float width = 1;
 
-	switch (resolutionIndex)
-	{
-		case 1:
-			SetWindowSize(1024 , 768);
-			break;
-		case 2:
-			SetWindowSize(1600, 900);
-			break;
-		case 3:
-			SetWindowSize(1920 , 1080);
-			break;
-		case 4:
-			SetWindowSize(2560 , 1440);
-			break;
-	}
+		screenSize= { width, height };
 	
 
 }
@@ -98,7 +59,6 @@ void drawOptions()
 {
 	ClearBackground(BLACK);
 	drawButton(fullScreen);
-	drawButton(changeScreen);
 	drawButton(backButton);
 
 }
